76815cbe0070d673394f649e141f8703cb54a8f3
Brice G <bricegoudalo@gmail.com>
2023-07-18 21:33:33 +0200

    add assignment length and tags

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -5,9 +5,17 @@ export default {
 
     template: `
         <section v-show="assignments.length">
-            <h2 class="font-bold mt-5">{{ title }}</h2>
 
-            <ul class="border border-gray-600 divide-y divide-gray-600">
+            <h2 class="font-bold mt-5">
+                {{ title }}
+                <span>( {{ assignments.length }} )</span>
+            </h2>
+            
+            <div class="flex gap-2 mt-4">
+                <button v-for="tag in tags" class="border rounded px-1 py-px text-xs">{{ tag }}</button>
+            </div>
+
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-4">
                 <assignment 
                     v-for="assignment in assignments"
                     :key="assignment.id"
@@ -24,5 +32,11 @@ export default {
     props: {
         assignments: Array,
         title: String
+    },
+
+    computed: {
+        tags() {
+            return ['Science', 'Maths', 'Computer Science'];
+        }
     }
 }
